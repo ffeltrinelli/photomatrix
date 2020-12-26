5e86217b0e0d65535445a2e8d0c6f9e6f763b743
Francesco Feltrinelli <44259532+ffeltrinelli@users.noreply.github.com>
2020-12-26 17:09:42 +0100

    Simplified image diff by using ImageChops.difference

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,7 +1,4 @@
-import math
-import operator
-from functools import reduce
-from PIL import Image
+from PIL import Image, ImageChops
 import pytest
 from pathlib import Path
 
@@ -43,32 +40,15 @@ class ImageDiff:
     """Check the difference between test images."""
 
     @staticmethod
-    def _rms_diff(im1, im2):
-        """Calculate the root-mean-square difference between two images
-        Taken from: https://stackoverflow.com/a/29268346/448915
-        """
-        h1 = im1.histogram()
-        h2 = im2.histogram()
-
-        def mean_sqr(a, b):
-            if not a:
-                a = 0.0
-            if not b:
-                b = 0.0
-            return (a - b) ** 2
-
-        return math.sqrt(reduce(operator.add, map(mean_sqr, h1, h2)) / (im1.size[0] * im1.size[1]))
-
-    @staticmethod
-    def assert_equal(actual_img_path, expected_img_path, max_threshold=0.0):
+    def assert_equal(actual_img_path, expected_img_path):
         """Checks whether the actual image produced by the program is equal to
         a known expected image.
         """
-        actual_img = Image.open(actual_img_path)
-        expected_img = Image.open(expected_img_path)
-        rms_value = ImageDiff._rms_diff(actual_img, expected_img)
-        if rms_value > max_threshold:
-            pytest.fail(f'Image {actual_img.filename} is different from expected {expected_img.filename}')
+        actual_img = Image.open(actual_img_path).convert('RGB')
+        expected_img = Image.open(expected_img_path).convert('RGB')
+        diff = ImageChops.difference(actual_img, expected_img)
+        if diff.getbbox():
+            pytest.fail(f'Image {actual_img_path} is different from expected {expected_img_path}')
 
 
 @pytest.fixture
